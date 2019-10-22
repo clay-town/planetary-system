4f16ed625e3ab149656538da9c66b33571298630
clay-town <justinmccune@icloud.com>
2019-10-22 13:05:37 -0700

    mostly finished

--- planetarySystem.go
+++ planetarySystem.go
@@ -1,4 +1,5 @@
 //This is my first go program!
+// made with the help of Golang documenation and tutorialedge.net
 package main
 
 import (
@@ -6,11 +7,24 @@ import (
         "bufio"
         "os"
         "strings"
+        "encoding/json"
+        "io/ioutil"
+        "time"
 )
+import "math/rand"
+
+type Planets struct {
+  Planets []Planet `json:"planets"`
+}
+type Planet struct {
+  Name          string  `json:"name"`
+  Description   string  `json: "description"`
+}
 
 func main(){
+  rand.Seed(time.Now().UnixNano())
   var reader = bufio.NewReader(os.Stdin)
-
+  rLoc := rand.Intn(8)
 
   var iName = greeting(reader)
   fmt.Print("Nice to meet you, ",iName," My name is Eliza, I'm an old friend of Alexa. \nShall I randomly choose a planet for you to visit? (Y or N)\n")
@@ -18,12 +32,60 @@ func main(){
   switch rand{
   case true:
     // do true things
-    fmt.Print("true\n")
+    name, desc := readJsonFile(rLoc)
+    printStuff(name, desc)
+
   case false:
     // do false things
-    fmt.Print("false\n")
+    fmt.Print("Name the planet you would like to visit\n")
+    var loc = validatePlanet(reader)
+    name, desc := readJsonFile(loc)
+    printStuff(name, desc)
   }
 }
+
+func printStuff(name string, desc string) {
+  fmt.Println("Welcome to " + name + "...")
+  fmt.Println(desc)
+}
+
+func validatePlanet(reader *bufio.Reader) int{
+  iPlanet, _ := reader.ReadString('\n')
+  switch strings.TrimRight(iPlanet, "\n") {
+  case "Mercury","mercury":
+    return 0
+  case "Venus","venus":
+    return 1
+  case "Earth", "earth":
+    return 2
+  case "Mars", "mars":
+    return 3
+  case "Jupiter", "jupiter":
+    return 4
+  case "Saturn", "saturn":
+    return 5
+  case "Uranus", "uranus":
+    return 6
+  case "Neptune", "neptune":
+    return 7
+  case "Pluto", "pluto":
+    return 8
+  }
+  fmt.Print("Please enter a valid planet name \n")
+  return validatePlanet(reader)
+}
+
+func readJsonFile(loc int) (string, string){
+  jsonFile, _ := os.Open("planetarySystem.json")
+  byteValue, _ := ioutil.ReadAll(jsonFile)
+  var planets Planets
+  json.Unmarshal(byteValue, &planets)
+  defer jsonFile.Close()
+  return planets.Planets[loc].Name, planets.Planets[loc].Description
+}
+
+
+
 
 func greeting(reader *bufio.Reader) string{
   //var dest = "Traveling to Pluto...Arrived at Pluto. I don't care what they say - it's a planet\n"
